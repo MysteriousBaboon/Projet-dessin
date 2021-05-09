ff54bce98bf23064891305d78829c4c7365154eb
MysteriouBaboon <MysteriousBaboon@hotmail.com>
2021-05-09 20:14:24 +0200

    added random question through api

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,8 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question', 'answer', 'genre')
+    readonly_fields = ('id',)
+
 
 
 # Register your models here.
--- api/views.py
+++ api/views.py
@@ -1,10 +1,21 @@
 from rest_framework import viewsets
 from .serializers import QuestionSerializer
 from .models import Question
+import random
+
+# grab the max id in the database
+max_id = Question.objects.order_by('-id')[0].id
 
 
 class QuestionView(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
-    queryset = Question.objects.all()
 
+    # grab a random possible id. we don't know if this id does exist in the database, though
 
+    # return an object with that id, or the first object with an id greater than that one
+    # this is a fast lookup, because your primary key probably has a RANGE index.
+
+    def get_queryset(self):
+        random_id = random.randint(1, max_id)
+        print(random_id)
+        return Question.objects.all().filter(id= random_id)
